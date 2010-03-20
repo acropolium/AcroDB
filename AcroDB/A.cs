a340a0c9856197eda9b83604c5e0595e90cae09a
oleksiy <oleksiy.glib@gmail.com>
2010-03-20 19:58:40 +0200

    AcroContext helper added

---
+++ AcroDB/A.cs
@@ -0,0 +1,10 @@
+﻿namespace AcroDB
+{
+    public class A
+    {
+        public static AcroDataContext Context(object obj)
+        {
+            return ((AcroEntity)obj).AcroContext;
+        }
+    }
+}
